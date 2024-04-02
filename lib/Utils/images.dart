0074c3749ba91ca1d693c92a3a825bd7f0de580f
Vampie0 <alibhatti6382@gmail.com>
2024-04-02 14:14:05 +0500

    Adderd login screen

--- lib/Utils/images.dart
+++ lib/Utils/images.dart
@@ -1,4 +1,6 @@
 class Images {
-  static const String splashImage = "assets/splash.jpg";
   static const String splash = "assets/perfume1.jpg";
+  static const String login = "assets/login.jpg";
+  static const String signup = "assets/signup.jpg";
+  static const String logout = "assets/logout,jpg";
 }
